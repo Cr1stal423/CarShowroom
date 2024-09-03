9a2bbe7790fdb1cd1ec5b6dd8f7a97fdb54ead19
cr1sral423 <vlad.galyara@gmail.com>
2024-09-03 22:03:51 +0300

    add JpaAuditing

--- src/main/java/com/dealership/car/CarApplication.java
+++ src/main/java/com/dealership/car/CarApplication.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableAspectJAutoProxy
+@EnableJpaAuditing(auditorAwareRef = "")
 public class CarApplication  {
 	@Autowired
 	private ProductRepository productRepository;
