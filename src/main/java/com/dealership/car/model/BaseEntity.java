cc0d97da47d60f1a4cfc79c86e60ccc7d044dc68
whatIsLove <vlad.galyara@gmail.com>
2024-09-10 13:33:07 +0300

    Update JPA Auditing

--- src/main/java/com/dealership/car/model/BaseEntity.java
+++ src/main/java/com/dealership/car/model/BaseEntity.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -12,11 +16,15 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Column(name = "created_by")
+    @CreatedBy
     private String createdBy;
     @Column(name = "created_at")
+    @CreatedDate
     private LocalDateTime createdAt;
     @Column(name = "updated_by")
+    @LastModifiedBy
     private String updatedBy;
     @Column(name = "updated_at")
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
