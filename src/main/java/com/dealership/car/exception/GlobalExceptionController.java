045a8999dc884cc5f931a909c003b7720e3e0189
whatIsLove <vlad.galyara@gmail.com>
2024-11-16 15:21:37 +0200

    update exception logic

--- src/main/java/com/dealership/car/exception/GlobalExceptionController.java
+++ src/main/java/com/dealership/car/exception/GlobalExceptionController.java
@@ -1,5 +1,6 @@
 package com.dealership.car.exception;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,9 +8,14 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalExceptionController {
     @ExceptionHandler(Exception.class)
-    public ModelAndView handleGlobalException(Exception ex){
+    public ModelAndView handleGlobalException(Exception ex) {
+        if (ex instanceof jakarta.validation.UnexpectedTypeException || ex instanceof MethodArgumentNotValidException) {
+            return null;
+        }
+
         ModelAndView modelAndView = new ModelAndView("error.html");
         modelAndView.addObject("exception", ex);
         return modelAndView;
+
     }
 }
